4cf49bde902570ad7239eb3942f2f5f30ace0b44
Sidharth <sidharthinfernal@gmail.com>
2024-10-20 22:25:32 +0530

    url correction-2

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,7 +17,7 @@ export default function Login() {
           onClick={async () => {
             await signIn.social({
               provider: "github",
-              callbackURL: "https://testimonial-vert.vercel.app/",
+              callbackURL: "https://testimonial-git-sidharth-sidhuiwnls-projects.vercel.app/",
             });
           }}
         >
