dc7acd56c990094c4e9625a1ac9c049241042d96
Sidharth <sidharthinfernal@gmail.com>
2024-10-21 18:49:08 +0530

    redirects

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -11,7 +11,7 @@ export const auth = betterAuth({
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-      redirectURI: process.env.BASE_URL+'/api/auth/callback/github'
+      
     },
   },
 });
--- components/Signout.tsx
+++ components/Signout.tsx
@@ -1,14 +1,17 @@
 import { signOut } from "@/app/lib/auth-client";
-
+import { useRouter } from "next/navigation";
 
 export default function SignOut() {
+  const router = useRouter();
+
   return (
     <button
       onClick={async () => {
         await signOut();
+        router.push("/login"); 
       }}
     >
-      <span>LogOut</span>
+      <span className="font-medium">Logout</span>
     </button>
   );
 }
