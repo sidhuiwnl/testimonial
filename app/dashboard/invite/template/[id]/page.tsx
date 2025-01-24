ebc88a2addd133ec828a710f3bf18d395c92a07c
Sidharth <sidharthinfernal@gmail.com>
2025-01-24 13:01:50 +0530

    style correction

--- app/dashboard/invite/template/[id]/page.tsx
+++ app/dashboard/invite/template/[id]/page.tsx
@@ -37,8 +37,6 @@ export default function Page() {
                         >
                             Write a Review 👉</Button>
                     </div>
-
-
                 </CardContent>
 
             </Card>
