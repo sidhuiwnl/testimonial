4ef3b1847efa9e3ddcd1de1dc8ca071753f1927f
Sidharth <sidharthinfernal@gmail.com>
2025-02-02 19:16:35 +0530

    minor change

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,6 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
+
 const f = createUploadthing();
 
 const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
